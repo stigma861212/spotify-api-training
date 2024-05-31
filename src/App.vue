02ebfb4031c70eb78e45d6e5959cc1249decebdf
stigma861212 <stigma861212@gmail.com>
2024-05-31 19:34:49 +0800

    Add component error

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script lang="ts">
+// FIXME:
+// import Titlebar from './components/Titlebar.vue';
 </script>
 
 <template>
+  <Titlebar />
   <h1>💖 Hello index World!Spotify.</h1>
   <p>Welcome to your Electron application.</p>
   <button id="loginButton">Login with Spotify</button>
---
+++ src/components/Titlebar.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Titlebar",
+});
+</script>
+
+<template>
+</template>
+
+<style>
+/* #title-bar {
+  color: red;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 1920px;
+  height: 30px;
+} */
+</style>
