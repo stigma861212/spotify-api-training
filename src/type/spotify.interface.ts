da1784cf1cdecc3db87816b694010a97ff7ab092
stigma861212 <stigma861212@gmail.com>
2024-06-13 11:03:58 +0800

    IProfile definition

--- src/type/spotify.interface.ts
+++ src/type/spotify.interface.ts
@@ -4,12 +4,19 @@
  * https://developer.spotify.com/documentation/web-api/reference/get-current-users-profile
  */
 export interface IProfile {
-    country: string
-    display_name: string
-    email: string
-    explicit_content: object
-    external_urls: object
-    followers: object
+    country: string;
+    display_name: string;
+    email: string;
+    explicit_content: {
+        filter_enabled: boolean,
+        filter_locked: boolean
+    },
+    external_urls: {
+        spotify: string;
+    };
+    followers: {
+        href: string; total: number;
+    };
     href: string
     id: string
     images: Array<IPicture>
